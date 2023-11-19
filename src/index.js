497bf43f45f82468bedb0d49d25ea540c10182b8
jay <38236622+JayJamieson@users.noreply.github.com>
2023-11-20 00:30:55 +1300

    base nodejs container runtime implementation

--- src/index.js
+++ src/index.js
@@ -8,6 +8,20 @@ const handler = process.argv[2];
 console.log(`Executing '${handler}' in function directory '${appDir}'`);
 
 async function run(appDir, handler) {
+  // setup service client
+
+  // setup error callback handlers
+
+  // setup termination handlers
+
+  // setup uncaughtException
+  // setup unhandledRejection
+
+  // setup before exit handler
+
+  // load user function
+
+  // run event reader loop
 }
 
 await run(appDir, handler);
